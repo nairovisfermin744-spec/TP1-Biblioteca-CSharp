3d08e8fbdbe09b966f5e497dda1fc043633dfaf9
nairovisfermin744-spec <nairovisfermin744@gmail.com>
2026-09-17 09:07:21 -0300

    Agregar clase Libro

---
+++ Libro.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Libro
+{
+    private string titulo;
+    private string autor;
+    private string editorial;
+
+    public Libro(string titulo, string autor, string editorial)
+    {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+    }
+
+    public string GetTitulo()
+    {
+        return titulo;
+    }
+
+    public override string ToString()
+    {
+        return $"{titulo} - {autor} - {editorial}";
+    }
+}
